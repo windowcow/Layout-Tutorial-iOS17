ed689327798654bdb0bac2261b136d646dbaccf6
windowcow <strongwindowcow@gmail.com>
2023-12-16 13:58:56 +0900

    Initial Commit

---
+++ Layout-Tutorial-iOS17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Layout-Tutorial-iOS17
+//
+//  Created by windowcow on 12/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Layout-Tutorial-iOS17/Layout_Tutorial_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  Layout_Tutorial_iOS17App.swift
+//  Layout-Tutorial-iOS17
+//
+//  Created by windowcow on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Layout_Tutorial_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
